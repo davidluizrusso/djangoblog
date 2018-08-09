4f50bef9806c6cff5ea12ca755fb54e7b6c7dc45
davidluizrusso <david.luiz.russo@gmail.com>
2018-08-08 18:46:02 -0700

    Got front page of application working

--- djangoblog/mysite/myblog/views.py
+++ djangoblog/mysite/myblog/views.py
@@ -13,11 +13,8 @@ def stub_view(request, *args, **kwargs):
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
 
-
 def list_view(request):
     published = Post.objects.exclude(published_date__exact=None)
     posts = published.order_by('-published_date')
-    template = loader.get_template('list.html')
     context = {'posts': posts}
-    body = template.render(context)
-    return HttpResponse(body, content_type="text/html")
+    return render(request, 'list.html', context)
